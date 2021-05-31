10f8e7920e939dee939aa356b5afa5c59ef69251
Jared <jmezzbmxer@live.ca>
2021-05-31 17:22:10 -0400

    moved bootstrap JS form validation to seperate file, added navbar, added search bar for plates & corresponding routes

--- app.js
+++ app.js
@@ -7,23 +7,28 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 // form submission assigned to using json
 app.use(express.urlencoded({ extended: true }));
 // pass in body as json on each request
 app.use(express.json());
 
-// shoot report routing
-const shootRoutes = require('./routes/shoot');
-app.use('/shoot', shootRoutes);
-
 app.get("/", (req, res) => {
     res.render('home');
 })
+
+// shoot report routing
+const shootRoutes = require('./routes/shoot');
+app.use('/shoot', shootRoutes);
 
 app.post("/contact", async(req, res) => {
     console.log(req.body);
     res.redirect("/");
 })
+
+
 
 app.listen(3000, function() {
     console.log("Server listening on Port 3000");
--- routes/shoot.js
+++ routes/shoot.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-// default path
-// 
-router.get("/", (req, res) => {
-    res.render("shoots/index");
+// Search shoot route
+router.post("/", async(req, res) => {
+    const { enteredPlate } = req.body ;
+    res.redirect(`/shoot/${ enteredPlate }`);
+})
+
+// specific route for showing custom page
+router.get("/:userKey", async(req, res) => {
+    const { userKey } = req.params;
+    res.render("shoots/customPage", { userKey });
 })
 
 module.exports = router;
